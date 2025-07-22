2c2577391a0f986082cddbd81277a2f52f67ae77
vigvij <vignesh.vijayakumar@hidglobal.com>
2025-07-22 18:40:54 +0530

    product fetching logic

--- src/lib/utils/endpoint.js
+++ src/lib/utils/endpoint.js
@@ -1,2 +1,3 @@
 export const BASEURL = 'http://localhost:3000/api';
 export const LOGIN_ENDPOINT = `${BASEURL}/auth-service/login`;
+export const PRODUCTS_ENDPOINT = `${BASEURL}/product-service/products`;
--- src/routes/products/+page.svelte
+++ src/routes/products/+page.svelte
@@ -1,1 +1,36 @@
+<script>
+    import { PRODUCTS_ENDPOINT } from "$lib/utils/endpoint";
+    import { onMount } from "svelte";
+
+    
+    
+    let products = []
+
+    const getProducts = async () => {
+         const res = await fetch(PRODUCTS_ENDPOINT);
+         const data = await res.json();
+        if(res.ok){
+            products = data?.data
+        }
+    }
+
+    onMount(async()=>{
+        
+        try{
+            getProducts();
+        }catch(err){
+            console.log(err)
+        }
+    })
+
+</script>
+
+
+{#each products as product }
+    <h4>{product.name}</h4>
+    <p>{product.description}</p>
+    <p>{product.price}</p>
+    <p>{product.stock}</p>
+{/each}
+
 <h1>Products</h1>
