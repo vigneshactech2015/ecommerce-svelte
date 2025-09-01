87e608d1450d9cdc7a71538d8f1af5665286c923
vigvij <vignesh.vijayakumar@hidglobal.com>
2025-09-01 10:07:32 +0530

    s3 changes

--- svelte.config.js
+++ svelte.config.js
@@ -7,12 +7,15 @@ const config = {
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
 		adapter: adapter({
-      // default options:
-      pages: 'build',
-      assets: 'build',
-      fallback: 'error.html',
-      precompress: false
-    })
+			// default options:
+			pages: 'build',
+			assets: 'build',
+			fallback: 'error.html',
+			precompress: false
+		}),
+		prerender: {
+			entries: ['*']
+		}
 	}
 };
 
